666aa90af650818674fe9861dba80f39d634d33c
Reto Buerki <reet@codelabs.ch>
2015-02-16 10:38:00 +0100

    hw: Add dump_interrupt_info function to idt.cc
    This helper function is used to dump information about an exception or
    interrupt.

--- repos/base-hw/src/core/spec/x86_64/idt.cc
+++ repos/base-hw/src/core/spec/x86_64/idt.cc
@@ -1,3 +1,5 @@
+#include <base/printf.h>
+
 #include "idt.h"
 
 using namespace Genode;
@@ -37,4 +39,44 @@ void Idt::load()
 {
 	asm volatile ("lidt %0" : : "m" (Descriptor (sizeof (_table) - 1,
 				  reinterpret_cast<uint64_t>(_table))));
+}
+
+
+class CPU_regs {
+
+	public:
+		uint64_t _rax, _rbx, _rcx, _rdx, _rdi, _rsi, _rbp, _r08, _r09, _r10,
+				 _r11, _r12, _r13, _r14, _r15;
+
+};
+
+
+class ISR_context {
+	public:
+		CPU_regs _gpr;
+
+		uint64_t _vector, _error_code, _rip, _cs, _rflags, _rsp, _ss;
+};
+
+
+extern "C" void dump_interrupt_info(ISR_context context)
+{
+	PERR("unhandled exception/interrupt occurred with vector %d",
+		 (int)context._vector);
+
+	PDBG("rip %16llx cs %04x", context._rip, (int)context._cs);
+	PDBG("rsp %16llx ss %04x", context._rsp, (int)context._ss);
+	PDBG("rax %16llx rbx %16llx rcx %16llx",
+		 context._gpr._rax, context._gpr._rbx, context._gpr._rcx);
+	PDBG("rdx %16llx rsi %16llx rdi %16llx",
+		 context._gpr._rdx, context._gpr._rsi, context._gpr._rdi);
+	PDBG("rbp %16llx r08 %16llx r09 %16llx",
+		 context._gpr._rbp, context._gpr._r08, context._gpr._r09);
+	PDBG("r10 %16llx r11 %16llx r12 %16llx",
+		 context._gpr._r10, context._gpr._r11, context._gpr._r12);
+	PDBG("r13 %16llx r14 %16llx r15 %16llx",
+		 context._gpr._r13, context._gpr._r14, context._gpr._r15);
+	PDBG("efl %16llx", context._rflags);
+
+	while (true) {}
 }
