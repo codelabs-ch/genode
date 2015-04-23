6ef8930c6057c4c4727532c0a65ee7c9fc8d0091
Reto Buerki <reet@codelabs.ch>
2015-04-23 13:12:05 +0200

    hw_x86_64_muen: Add initial pagetables file
    The initial pagetables for hw_x86_64_muen specify an identity mapping
    from 2MiB to 4MiB plus mappings for the paravirt pages (sinfo, timer,
    ...).

---
+++ repos/base-hw/src/core/spec/x86_64_muen/kernel/crt0_translation_table.s
@@ -0,0 +1,49 @@
+/*
+ * \brief   Initial pagetables for x86_64_muen
+ * \author  Adrian-Ken Rueegsegger
+ * \author  Reto Buerki
+ * \date    2015-04-22
+ */
+
+/*
+ * Copyright (C) 2015 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+.include "macros.s"
+
+.data
+
+	/*****************************************
+	 ** Identity mapping from 2MiB to 4MiB  **
+	 ** plus mappings for Muen pvirt pages  **
+	 *****************************************/
+
+	/* PML4 */
+	.p2align MIN_PAGE_SIZE_LOG2
+	.global _kernel_pml4
+	_kernel_pml4:
+	.quad _kernel_pdp + 0xf
+	.fill 511, 8, 0x0
+
+	/* PDP */
+	.p2align MIN_PAGE_SIZE_LOG2
+	_kernel_pdp:
+	.quad _kernel_pd + 0xf
+	.fill 55, 8, 0x0
+	.quad _kernel_pd_pvirt + 0xf
+	.fill 455, 8, 0x0
+
+	/* PD */
+	.p2align MIN_PAGE_SIZE_LOG2
+	_kernel_pd:
+	.quad 0
+	.quad 0x20018f
+	.fill 510, 8, 0x0
+
+	.p2align MIN_PAGE_SIZE_LOG2
+	_kernel_pd_pvirt:
+	.quad 0xe0000018f
+	.fill 511, 8, 0x0
