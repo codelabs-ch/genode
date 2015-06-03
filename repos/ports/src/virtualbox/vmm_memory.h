2e2209dbd7a32b2863d4587eee07646559af0639
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-06-03 09:50:36 +0200

    !WIP: Muen-specific IO mem region handling

--- repos/ports/src/virtualbox/vmm_memory.h
+++ repos/ports/src/virtualbox/vmm_memory.h
@@ -26,12 +26,13 @@
 #include <base/env.h>
 #include <base/lock.h>
 #include <util/list.h>
-#include <os/attached_ram_dataspace.h>
+#include <os/attached_io_mem_dataspace.h>
 
 #define PAGE_SIZE BACKUP_PAGESIZE
 
 /* VirtualBox includes */
 #include <VBox/vmm/pgm.h>
+#include <VBox/vmm/pdmdev.h>
 
 
 class Vmm_memory
@@ -41,13 +42,14 @@ class Vmm_memory
 	typedef Genode::Ram_session            Ram_session;
 	typedef Genode::Rm_session             Rm_session;
 	typedef Genode::size_t                 size_t;
+	typedef Genode::addr_t                 addr_t;
 	typedef Genode::Lock                   Lock;
-	typedef Genode::Attached_ram_dataspace Attached_ram_dataspace;
+	typedef Genode::Attached_io_mem_dataspace Attached_io_mem_dataspace;
 	typedef Genode::List<Region>           Region_list;
 
 	private:
 
-		struct Region : Region_list::Element, Attached_ram_dataspace
+		struct Region : Region_list::Element, Attached_io_mem_dataspace
 		{
 			PPDMDEVINS           pDevIns;
 			unsigned const       iRegion;
@@ -55,15 +57,63 @@ class Vmm_memory
 			PFNPGMR3PHYSHANDLER  pfnHandlerR3;
 			void                *pvUserR3;
 			PGMPHYSHANDLERTYPE   enmType;
+			size_t				 region_size;
+
+			addr_t _phys_base()
+			{
+				struct Region_info
+				{
+					addr_t base;
+					size_t size;
+				};
+				static unsigned counter;
+				static Region_info regions[] = {
+					/* RAM phys 0x0 */
+					{ 0x800000000, 0x10000000 },
+					/* pcbios phys 0xe1000 */
+					{ 0x810000000, 0x1000 },
+					/* pcbios phys 0xf0000*/
+					{ 0x820000000, 0x10000 },
+					/* pcbios 0xffff0000 */
+					{ 0x830000000, 0x10000 },
+					/* VMMDev */
+					{ 0x840000000, 0x400000 },
+					/* VMMDev */
+					{ 0x850000000, 0x4000 },
+					/* vga */
+					{ 0x860000000, 0xc00000 },
+					/* vga phys 0xc0000 */
+					{ 0x870000000, 0x9000 },
+					/* acpi phys e0000 */
+					{ 0x880000000, 0x1000 },
+					{ 0x0, 0x0 },
+				};
+
+				Region_info cur_region = regions[counter++];
+
+				if (cur_region.size == 0)
+				{
+					PERR("Region size is zero!!!");
+					return 0;
+				}
+				return cur_region.base;
+			}
+
+			size_t size() const { return region_size; }
 
 			Region(Ram_session &ram, size_t size, PPDMDEVINS pDevIns,
 			           unsigned iRegion)
 			:
-				Attached_ram_dataspace(&ram, size),
+				Attached_io_mem_dataspace(_phys_base(), size),
 				pDevIns(pDevIns),
 				iRegion(iRegion),
-				vm_phys(0), pfnHandlerR3(0), pvUserR3(0)
-			{ }
+				vm_phys(0), pfnHandlerR3(0), pvUserR3(0),
+				region_size(size)
+			{
+				PDBG("Virt address=0x%p, size 0x%zx, dev=%s ",
+						local_addr<char>(), size,
+		               pDevIns && pDevIns->pReg ? pDevIns->pReg->szName : 0);
+			}
 		};
 
 		Lock        _lock;
