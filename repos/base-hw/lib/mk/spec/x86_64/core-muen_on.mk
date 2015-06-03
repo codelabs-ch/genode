93ab9f9de7a207a65e678841934ea69a47bfa23e
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2015-06-03 11:07:39 +0200

    hw_x86_64_muen: implement VM service skeleton

--- repos/base-hw/lib/mk/spec/x86_64/core-muen_on.mk
+++ repos/base-hw/lib/mk/spec/x86_64/core-muen_on.mk
@@ -17,6 +17,9 @@ SRC_CC += spec/x86_64/muen/kernel/cpu_exception.cc
 SRC_CC += spec/x86_64/muen/kernel/thread_exception.cc
 SRC_CC += spec/x86_64/muen/platform_support.cc
 SRC_CC += spec/x86_64/muen/sinfo.cc
+SRC_CC += spec/x86_64/muen/kernel/vm.cc
+SRC_CC += spec/x86_64/muen/platform_services.cc
+SRC_CC += kernel/vm_thread_on.cc
 
 # include less specific configuration
 include $(REP_DIR)/lib/mk/spec/x86_64/core.inc
