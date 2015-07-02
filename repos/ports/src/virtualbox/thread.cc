8067fd22584ef4c8ebffbbf1d03251be2b9e56b4
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-07-02 23:56:41 +0200

    vbox: Set reference account of Cpu sessions
    This will enable quota trading between the various threads, which is a
    prerequisite for priority application on base-hw.

--- repos/ports/src/virtualbox/thread.cc
+++ repos/ports/src/virtualbox/thread.cc
@@ -56,7 +56,7 @@ static Genode::Cpu_connection * cpu_connection(RTTHREADTYPE type) {
 	Genode::snprintf(data, 16, "vbox %u", type);
 
 	con[type - 1] = new (env()->heap()) Cpu_connection(data, prio);
-
+	con[type - 1]->ref_account(env()->cpu_session_cap());
 	return con[type - 1];
 }
 
