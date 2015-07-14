d5e2237332abed1a2a557a74943699f60cc7976e
Reto Buerki <reet@codelabs.ch>
2015-07-14 18:25:05 +0200

    base_hw: Switch x86 LAPIC timer to TSC deadline mode
    WIP

--- repos/base-hw/src/core/include/spec/x86/timer.h
+++ repos/base-hw/src/core/include/spec/x86/timer.h
@@ -45,6 +45,8 @@ class Genode::Timer : public Mmio
 			PIT_CH2_DATA   = 0x42,
 			PIT_CH2_GATE   = 0x61,
 			PIT_MODE       = 0x43,
+
+			IA32_TSC_DEADLINE = 0x6e0,
 		};
 
 		/* Timer registers */
@@ -58,48 +60,40 @@ class Genode::Timer : public Mmio
 		struct Tmr_initial : Register <0x380, 32> { };
 		struct Tmr_current : Register <0x390, 32> { };
 
-		uint32_t _tics_per_ms = 0;
+		uint64_t _tics_per_ms = 0;
 
-		/* Measure LAPIC timer frequency using PIT channel 2 */
-		uint32_t _pit_calc_timer_freq(void)
+		static inline uint64_t rdtsc()
 		{
-			uint32_t t_start, t_end;
+			uint32_t lo, hi;
+			asm volatile("rdtsc" : "=a" (lo), "=d" (hi));
+			return (uint64_t)hi << 32 | lo;
+		}
 
-			/* Set channel gate high and disable speaker */
-			outb(PIT_CH2_GATE, (inb(0x61) & ~0x02) | 0x01);
+		static inline void wrmsr(unsigned msr, uint64_t val)
+		{
+			asm volatile("wrmsr" : : "a" (static_cast<uint32_t>(val)), "d" (val >> 32), "c" (msr));
+		}
 
-			/* Set timer counter (mode 0, binary count) */
-			outb(PIT_MODE, 0xb0);
-			outb(PIT_CH2_DATA, PIT_SLEEP_TICS & 0xff);
-			outb(PIT_CH2_DATA, PIT_SLEEP_TICS >> 8);
-
-			write<Tmr_initial>(~0U);
-
-			t_start = read<Tmr_current>();
-			while ((inb(PIT_CH2_GATE) & 0x20) == 0)
-			{
-				asm volatile("pause" : : : "memory");
-			}
-			t_end = read<Tmr_current>();
-
-			write<Tmr_initial>(0);
-
-			return (t_start - t_end) / PIT_SLEEP_MS;
+		static inline uint64_t rdmsr(unsigned msr)
+		{
+			uint32_t low, high;
+			asm volatile ("rdmsr" : "=a" (low), "=d" (high) : "c" (msr));
+			return static_cast<uint64_t>(static_cast<uint64_t>(high) << 32 | low);
 		}
 
 	public:
 
 		Timer() : Mmio(Board::MMIO_LAPIC_BASE)
 		{
-			/* Enable LAPIC timer in one-shot mode */
+			/* Enable LAPIC timer in TSC deadline mode */
 			write<Tmr_lvt::Vector>(Board::TIMER_VECTOR_KERNEL);
 			write<Tmr_lvt::Delivery>(0);
 			write<Tmr_lvt::Mask>(0);
-			write<Tmr_lvt::Timer_mode>(0);
+			write<Tmr_lvt::Timer_mode>(2);
 
 			/* Calculate timer frequency */
-			_tics_per_ms = _pit_calc_timer_freq();
-			PINF("LAPIC: timer frequency %u kHz", _tics_per_ms);
+			_tics_per_ms = 2894674;
+			PINF("TIMER LAPIC: timer frequency %llu kHz", _tics_per_ms);
 		}
 
 		/**
@@ -118,19 +112,24 @@ class Genode::Timer : public Mmio
 			return Board::TIMER_VECTOR_KERNEL;
 		}
 
-		inline void start_one_shot(uint32_t const tics, unsigned)
+		inline void start_one_shot(uint64_t const tics, unsigned)
 		{
-			write<Tmr_initial>(tics);
+			wrmsr(IA32_TSC_DEADLINE, rdtsc() + tics);
+			//PDBG("TIMER MSR programmed to %llu", rdmsr(IA32_TSC_DEADLINE));
 		}
 
-		uint32_t ms_to_tics(unsigned const ms)
+		uint64_t ms_to_tics(unsigned const ms)
 		{
+			//PDBG("TIMER %u ms are %llu tics", ms, ms * _tics_per_ms);
 			return ms * _tics_per_ms;
 		}
 
-		unsigned value(unsigned)
+		uint64_t value(unsigned)
 		{
-			return read<Tmr_current>();
+			uint64_t now = rdtsc();
+			uint64_t deadline = rdmsr(IA32_TSC_DEADLINE);
+
+			return deadline == 0 ? deadline : deadline - now;
 		}
 };
 
