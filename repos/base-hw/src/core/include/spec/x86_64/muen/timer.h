d8a0e1b85b6e4227f3bf0097f60fbbea2a220b17
Reto Buerki <reet@codelabs.ch>
2016-04-21 14:56:25 +0200

    hw_x86_64_muen: Use timed events to implement timer
    Switch kernel timer driver to timed event interface. The base-hw kernel
    expects a configured self-event with ID 31 and target vector 32 to be
    present in the system policy.

--- repos/base-hw/src/core/include/spec/x86_64/muen/timer.h
+++ repos/base-hw/src/core/include/spec/x86_64/muen/timer.h
@@ -39,13 +39,13 @@ class Genode::Timer
 
 		uint64_t _tics_per_ms;
 
-		struct Subject_timer
+		struct Subject_timed_event
 		{
-			uint64_t value;
-			uint8_t  vector;
+			uint64_t tsc_trigger;
+			uint8_t  event_nr :5;
 		} __attribute__((packed));
 
-		struct Subject_timer * _timer_page;
+		struct Subject_timed_event * _event_page;
 
 		inline uint64_t rdtsc()
 		{
@@ -61,22 +61,23 @@ class Genode::Timer
 		Timer() : _tics_per_ms(Sinfo::get_tsc_khz())
 		{
 			struct Sinfo::Memregion_info region;
-			if (!Sinfo::get_memregion_info("timer", &region)) {
-				PERR("muen-timer: Unable to retrieve time memory region");
+			if (!Sinfo::get_memregion_info("timed_event", &region)) {
+				PERR("muen-timer: Unable to retrieve timed event region");
 				throw Invalid_region();
 			}
 
-			_timer_page = (Subject_timer *)region.address;
-			_timer_page->vector = Board::TIMER_VECTOR_KERNEL;
-			PINF("muen-timer: page @0x%llx, frequency %llu kHz, vector %u",
-			     region.address, _tics_per_ms, _timer_page->vector);
+			_event_page = (Subject_timed_event *)region.address;
+			_event_page->event_nr = Board::TIMER_EVENT_KERNEL;
+			PINF("muen-timer: page @0x%llx, frequency %llu kHz, event %u",
+			     region.address, _tics_per_ms, _event_page->event_nr);
 		}
 
 		static unsigned interrupt_id(int) {
 			return Board::TIMER_VECTOR_KERNEL; }
 
 		inline void start_one_shot(time_t const tics, unsigned) {
-			_timer_page->value = rdtsc() + tics; }
+			_event_page->tsc_trigger = rdtsc() + tics; }
+		}
 
 		time_t tics_to_us(time_t const tics) const {
 			return (tics / _tics_per_ms) * 1000; }
@@ -89,9 +90,9 @@ class Genode::Timer
 		time_t value(unsigned)
 		{
 			const uint64_t now = rdtsc();
-			if (_timer_page->value != TIMER_DISABLED
-			    && _timer_page->value > now) {
-				return _timer_page->value - now;
+			if (_event_page->tsc_trigger != TIMER_DISABLED
+			    && _event_page->tsc_trigger > now) {
+				return _event_page->tsc_trigger - now;
 			}
 			return 0;
 		}
